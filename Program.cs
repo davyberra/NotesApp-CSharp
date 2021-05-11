1e4c1164690b9c09a972944e7a64d228ff9e3906
unknown <davyberra@gmail.com>
2021-05-10 20:39:52 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NotesApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("What's on your mind?");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string fileName = ("D:\\dotnet Projects\\NotesApp\\notes.json");
+                AddNote(input, fileName);
+            }
+
+        }
+
+        static void AddNote(string input, string fileName)
+        {
+            DateTime date = DateTime.Now;
+            Note note = new Note();
+            string dateString = date.ToString("F");
+            note.Date = dateString;
+            note.Content = input;
+            var serializer = new JsonSerializer();
+            using (var writer = new StreamWriter(fileName, true))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+
+                serializer.Serialize(jsonWriter, note);
+                
+            }
+        }
+    }
+}
